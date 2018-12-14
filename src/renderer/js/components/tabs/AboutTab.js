e1b11bb953ebc0b82d100d9eaa1f9bf922585132
Nick <nick.booth52@gmail.com>
2018-12-14 22:49:40 +0000

    Added some of the about page

--- src/renderer/js/components/tabs/AboutTab.js
+++ src/renderer/js/components/tabs/AboutTab.js
@@ -2,8 +2,21 @@ const AboutTab = Vue.component("ddmm-about-tab", {
     "template": `
 <div class="page-content">
     <div class="text-container">
-        some page content goes here
+        <img src="../images/ddmm.png" width="200" style="float: right; padding: 1em;">
+        
+        <h1>{{_("renderer.tab_about.title")}}</h1>
+        <p>{{_("renderer.tab_about.description")}}</p>
+        
+        <br>
+        
+        <h2>{{_("renderer.tab_about.title_disclaimer")}}</h2>
+        <p>{{_("renderer.tab_about.disclaimer_1")}}</p>
+        <br>
+        <p>{{_("renderer.tab_about.disclaimer_2")}}</p>
     </div>
 </div>
-        `
+        `,
+    "methods": {
+        "_": ddmm.translate,
+    }
 });
