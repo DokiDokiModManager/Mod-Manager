85c2dcf275ecc7ced9915be0ba9fb440ad0f3e7d
Nick <nick.booth52@gmail.com>
2018-12-14 13:13:03 +0000

    Added base layout for options tab

--- src/renderer/js/components/tabs/OptionsTab.js
+++ src/renderer/js/components/tabs/OptionsTab.js
@@ -5,7 +5,8 @@ const OptionsTab = Vue.component("ddmm-options-tab", {
         <div class="mod-viewer-mod-list">
             <template v-for="section in menu">
                 <div class="mod-view-mod-list-title">{{section.header}}</div>
-                <div v-for="item in section.contents" :class="{'mod-view-mod-list-entry': true, 'active': selected_option === item.id}" @click="selected_option = item.id">{{item.title}}</div>
+                <div v-for="item in section.contents" :class="{'mod-view-mod-list-entry': true, 'active': selected_option === item.id}" @click="selectOption(item.id)">{{item.title}}</div>
+                <br>
             </template>
         </div>
     </div>
@@ -20,6 +21,19 @@ const OptionsTab = Vue.component("ddmm-options-tab", {
                     "contents": [
                         {"title": ddmm.translate("renderer.tab_options.list.link_background"), "id": "background"}
                     ]
+                },
+                {
+                    "header": ddmm.translate("renderer.tab_options.list.header_enhancements"),
+                    "contents": [
+                        {"title": ddmm.translate("renderer.tab_options.list.link_sdk"), "id": "sdk"},
+                        {"title": ddmm.translate("renderer.tab_options.list.link_discord"), "id": "discord"}
+                    ]
+                },
+                {
+                    "header": ddmm.translate("renderer.tab_options.list.header_application"),
+                    "contents": [
+                        {"title": ddmm.translate("renderer.tab_options.list.link_storage"), "id": "storage"}
+                    ]
                 }
             ]
         }
@@ -28,7 +42,7 @@ const OptionsTab = Vue.component("ddmm-options-tab", {
         "_": ddmm.translate,
         "selectOption": function (option) {
             this.selected_option = option;
-            sessionStorage.setItem("options_last_id", id);
+            sessionStorage.setItem("options_last_id", option);
         }
     },
     "mounted": function () {
