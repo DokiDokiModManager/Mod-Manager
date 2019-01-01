b7d5326347a90449baec88c6ef33570032144f82
Nick <nick.booth52@gmail.com>
2019-01-01 23:05:00 +0000

    Remove all compiled TS and sourcemaps

--- src/main/sdk/LogClass.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var LogClass;
-(function (LogClass) {
-    LogClass["WARNING"] = "warning";
-    LogClass["ERROR"] = "error";
-})(LogClass = exports.LogClass || (exports.LogClass = {}));
-//# sourceMappingURL=LogClass.js.map
