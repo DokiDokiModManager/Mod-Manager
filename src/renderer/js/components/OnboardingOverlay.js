b6f02549dbcee1e3a74e9539478a0086b235fd47
Nick <nick.booth52@gmail.com>
2019-01-08 13:04:26 +0000

    Added some placeholders for onboarding

--- src/renderer/js/components/OnboardingOverlay.js
+++ src/renderer/js/components/OnboardingOverlay.js
@@ -1,12 +1,20 @@
 const OnboardingOverlay = Vue.component("ddmm-onboarding", {
     "template": `<div class="cover">
-    <p>This would be some text that shows you how to use DDMM</p>
-    <p>Maybe some images could go here as well.</p>
+    <h1>{{_("renderer.onboarding.title")}}</h1>
+    <p>{{_("renderer.onboarding.description_download")}}</p>
+    <br>
+    <p><button class="primary">{{_("renderer.onboarding.button_download")}}</button> <button class="secondary">{{_("renderer.onboarding.button_choose")}}</button></p>
+    <br>
+    <p>{{_("renderer.onboarding.heading_why")}}</p>
+    <div>{{_("renderer.onboarding.description_why")}}</div>
 </div>
     `,
     "data": function () {
         return {
 
         }
+    },
+    "methods": {
+        "_": ddmm.translate,
     }
 });
