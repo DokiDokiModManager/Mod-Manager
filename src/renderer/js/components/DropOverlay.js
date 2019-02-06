a769cad35020045b0546759ecbda95b50e1150ea
Nick Booth <nick.booth52@gmail.com>
2019-02-06 12:47:30 +0000

    Added localisation for drop cover

--- src/renderer/js/components/DropOverlay.js
+++ src/renderer/js/components/DropOverlay.js
@@ -2,7 +2,10 @@ const DropOverlay = Vue.component("ddmm-drop-overlay", {
     "template": `<div class="drop-overlay">
     <div class="info">
         <p><i class="fas fa-download fa-2x"></i></p>
-        <p>insert localised drag+drop text here</p>
+        <p>{{_("renderer.drop_cover.text")}}</p>
     </div>
-</div>`
+</div>`,
+    "methods": {
+        "_": ddmm.translate
+    }
 });
