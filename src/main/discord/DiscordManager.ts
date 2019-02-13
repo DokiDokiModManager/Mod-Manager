ba778e16cb2def1bb957372ad3e3c8f301abf407
Nick Booth <nick.booth52@gmail.com>
2019-02-13 09:27:43 +0000

    Potential fix for #54

--- src/main/discord/DiscordManager.ts
+++ src/main/discord/DiscordManager.ts
@@ -1,9 +1,15 @@
 import * as makeClient from "discord-rich-presence";
 import {app} from "electron";
 import Config from "../utils/Config";
+import Timeout = NodeJS.Timeout;
 
 export default class DiscordManager {
+
     private client: any;
+
+    private presenceUpdateTimer: Timeout;
+
+    private presence: RichPresenceData = null;
 
     constructor(appID: string) {
         if (Config.readConfigValue("discordEnabled")) {
@@ -13,22 +19,35 @@ export default class DiscordManager {
                 this.client = null;
                 console.log("Could not enable Rich Presence: " + e.message);
             });
+
+            this.presenceUpdateTimer = setInterval(() => {
+                this.sendPresence();
+            }, 10000);
         }
+    }
+
+    /**
+     * Sends the presence data to Discord
+     */
+    private sendPresence(): void {
+        if (!this.client) return;
+        this.client.updatePresence(this.presence);
     }
 
     /**
      * Sets Discord rich presence to the idle (not playing anything) status
      */
     public setIdleStatus(): void {
-        if (!this.client) return;
-        this.client.updatePresence({
+        this.presence = {
             details: "Managing Mods",
             startTimestamp: Date.now(),
             largeImageKey: "logo",
             smallImageKey: "idle",
             largeImageText: "Version " + app.getVersion(),
             smallImageText: "Not playing anything"
-        });
+        };
+
+        this.sendPresence();
     }
 
     /**
@@ -36,8 +55,7 @@ export default class DiscordManager {
      * @param installName The name of the installation
      */
     public setPlayingStatus(installName: string): void {
-        if (!this.client) return;
-        this.client.updatePresence({
+        this.presence = {
             details: "In Game",
             state: installName,
             startTimestamp: Date.now(),
@@ -45,7 +63,9 @@ export default class DiscordManager {
             smallImageKey: "playing",
             largeImageText: "Version " + app.getVersion(),
             smallImageText: "Playing DDLC"
-        });
+        };
+
+        this.sendPresence();
     }
 
     /**
@@ -53,7 +73,18 @@ export default class DiscordManager {
      */
     public shutdown(): void {
         if (!this.client) return;
-
+        clearTimeout(this.presenceUpdateTimer);
         this.client.disconnect();
     }
+}
+
+class RichPresenceData {
+    details?: string;
+    state?: string;
+    startTimestamp?: number;
+    endTimestamp?: number;
+    largeImageKey?: string;
+    smallImageKey?: string;
+    largeImageText?: string;
+    smallImageText?: string;
 }
