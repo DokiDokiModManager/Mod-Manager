a8307b7e867a1b490c556472d30949544b8c15ba
Nick Booth <nick.booth52@gmail.com>
2019-02-20 21:50:31 +0000

    Added socials, too tired for anything else

--- src/renderer/js/components/tabs/AboutTab.js
+++ src/renderer/js/components/tabs/AboutTab.js
@@ -13,10 +13,19 @@ const AboutTab = Vue.component("ddmm-about-tab", {
         <p>{{_("renderer.tab_about.disclaimer_1")}}</p>
         <br>
         <p>{{_("renderer.tab_about.disclaimer_2")}}</p>
+        
+        <br>
+        
+        <h2>{{_("renderer.tab_about.title_socials")}}</h2>
+        <p><a href="javascript:;" @click="openURL('https://doki.space/discord')"><i class="fab fa-discord fa-fw"></i> {{_("renderer.tab_about.social_discord")}}</a></p>
+        <p><a href="javascript:;" @click="openURL('https://twitter.com/DokiMod')"><i class="fab fa-twitter fa-fw"></i> {{_("renderer.tab_about.social_twitter")}}</a></p>
+        <p><a href="javascript:;" @click="openURL('https://www.reddit.com/message/compose?to=zuudo')"><i class="fab fa-reddit fa-fw"></i> {{_("renderer.tab_about.social_reddit")}}</a></p>
+        <p><a href="javascript:;" @click="openURL('mailto:zudo@doki.space')"><i class="fas fa-envelope fa-fw"></i> {{_("renderer.tab_about.social_email")}}</a></p>
     </div>
 </div>
         `,
     "methods": {
         "_": ddmm.translate,
+        "openURL": ddmm.app.openURL
     }
 });
