662edf6796c868dae0a624b5812b3e07cc55395d
Nick Booth <zudo@doki.space>
2020-06-01 15:09:24 +0100

    Fix janky cross-platform behaviour, part 2

--- src/main/archive/ArchiveConverter.ts
+++ src/main/archive/ArchiveConverter.ts
@@ -1,11 +1,10 @@
 import {mkdtempSync, removeSync} from "fs-extra";
-import {spawnSync, SpawnSyncReturns} from "child_process";
+import {execFileSync, spawnSync, SpawnSyncReturns} from "child_process";
 import {join as joinPath, sep as pathSep} from "path";
 import {app} from "electron";
 import {path7za} from "7zip-bin";
 import * as pathUnrar from "unrar-binaries";
 import Logger from "../utils/Logger";
-import {chmodSync} from "fs";
 
 export default class ArchiveConverter {
 
@@ -14,46 +13,27 @@ export default class ArchiveConverter {
 
         Logger.info("Archive Converter", "Converting " + tempDir + " to a zip file");
 
-        let extractOut: SpawnSyncReturns<string>;
+        let extractOut: string;
 
         if (!pathToArchive.endsWith(".rar")) {
             Logger.info("Archive Converter", "Using 7za to convert");
 
             // run 7zip to extract the archive
-            extractOut = spawnSync(path7za, ["x", pathToArchive, "-y", "-o" + tempDir]);
+            extractOut = execFileSync(path7za, ["x", pathToArchive, "-y", "-o" + tempDir]);
         } else {
             Logger.info("Archive Converter", "Using unrar to convert");
 
             // run unrar
-            if (process.platform !== "win32") {
-                chmodSync(pathUnrar, 0o774);
-            }
-            extractOut = spawnSync(pathUnrar, ["x", "-y", pathToArchive, tempDir]);
+            extractOut = execFileSync(pathUnrar, ["x", "-y", pathToArchive, tempDir]);
         }
 
-        Logger.debug("Archive Converter", "STDOUT: " + extractOut.stdout);
-        Logger.debug("Archive Converter", "STDERR: " + extractOut.status);
-
-        if (extractOut.error) {
-            throw extractOut.error;
-        }
-        if (extractOut.status !== 0) {
-            throw new Error("7-Zip or unrar exited with a non-zero exit code (" + extractOut.status + ")");
-        }
+        Logger.debug("Archive Converter", "STDOUT: " + extractOut);
 
         // run 7zip to compress to zip
         Logger.info("Archive Converter", "Recompressing " + tempDir);
-        const compressOut: SpawnSyncReturns<string> = spawnSync(path7za, ["a", output, "-y", tempDir + pathSep + "*"]);
+        const compressOut: string = execFileSync(path7za, ["a", output, "-y", tempDir + pathSep + "*"]);
 
-        Logger.debug("Archive Converter", "STDOUT: " + compressOut.stdout);
-        Logger.debug("Archive Converter", "STDERR: " + compressOut.status);
-
-        if (compressOut.error) {
-            throw compressOut.error;
-        }
-        if (compressOut.status !== 0) {
-            throw new Error("7-Zip exited with a non-zero exit code (" + compressOut.status + ")");
-        }
+        Logger.debug("Archive Converter", "STDOUT: " + compressOut);
 
         // delete the temp directory
         removeSync(tempDir);
