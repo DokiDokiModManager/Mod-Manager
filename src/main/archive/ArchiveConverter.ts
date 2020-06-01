83099636dc3275369672bc17623f236fcefbc236
Nick Booth <zudo@doki.space>
2020-06-01 14:14:01 +0100

    Fix janky cross-platform behaviour

--- src/main/archive/ArchiveConverter.ts
+++ src/main/archive/ArchiveConverter.ts
@@ -1,10 +1,11 @@
 import {mkdtempSync, removeSync} from "fs-extra";
 import {spawnSync, SpawnSyncReturns} from "child_process";
-import {join as joinPath} from "path";
+import {join as joinPath, sep as pathSep} from "path";
 import {app} from "electron";
 import {path7za} from "7zip-bin";
 import * as pathUnrar from "unrar-binaries";
 import Logger from "../utils/Logger";
+import {chmodSync} from "fs";
 
 export default class ArchiveConverter {
 
@@ -24,6 +25,9 @@ export default class ArchiveConverter {
             Logger.info("Archive Converter", "Using unrar to convert");
 
             // run unrar
+            if (process.platform !== "win32") {
+                chmodSync(pathUnrar, 0o774);
+            }
             extractOut = spawnSync(pathUnrar, ["x", "-y", pathToArchive, tempDir]);
         }
 
@@ -39,7 +43,7 @@ export default class ArchiveConverter {
 
         // run 7zip to compress to zip
         Logger.info("Archive Converter", "Recompressing " + tempDir);
-        const compressOut: SpawnSyncReturns<string> = spawnSync(path7za, ["a", output, "-y", tempDir + "\\*"]);
+        const compressOut: SpawnSyncReturns<string> = spawnSync(path7za, ["a", output, "-y", tempDir + pathSep + "*"]);
 
         Logger.debug("Archive Converter", "STDOUT: " + compressOut.stdout);
         Logger.debug("Archive Converter", "STDERR: " + compressOut.status);
